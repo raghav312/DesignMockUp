333e338cdb0fdafb9e314d79c4097ed6b29b73cf
raghav312 <raghavatri312@gmail.com>
2021-07-22 21:59:15 +0530

    design

--- app/src/main/java/com/example/designactivity/MainActivity.kt
+++ app/src/main/java/com/example/designactivity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,14 @@ class MainActivity : AppCompatActivity() {
             val bt2 = findViewById<ImageView>(R.id.imageView7)
             bt2.setOnClickListener {
                 setContentView(R.layout.onboard_trial_3)
+                val bt3= findViewById<ImageView>(R.id.imageView9)
+                bt3.setOnClickListener {
+                    setContentView(R.layout.onboard_trial_final)
+                    val ca = findViewById<LinearLayout>(R.id.btnCreateAccount)
+                    ca.setOnClickListener{println("Create Account Clicked")}
+                    val login = findViewById<LinearLayout>(R.id.btnLogin)
+                    login.setOnClickListener { println("Login clicked") }
+                }
             }
         }
 
