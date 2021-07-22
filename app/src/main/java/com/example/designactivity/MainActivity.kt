bc64c8966a2286a521158afbb65cef1076302da0
raghav312 <raghavatri312@gmail.com>
2021-07-22 20:16:09 +0530

    design

--- app/src/main/java/com/example/designactivity/MainActivity.kt
+++ app/src/main/java/com/example/designactivity/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.designactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboard_trial_1)
+
+        val bt = findViewById<ImageView>(R.id.imageView6)
+        bt.setOnClickListener {
+            setContentView(R.layout.onboard_trial_2)
+        }
+
+    }
+
+    override fun onBackPressed() {
         setContentView(R.layout.activity_main)
     }
 }
